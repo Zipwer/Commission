daf73d97dd241a6290d4f5185e6591c68a20771c
glebs <glebs@GLEB-PC>
2023-11-01 04:25:33 +0300

    Добавьте файлы проекта.

---
+++ GlebShirin2dayTaskA/GlebShirin2dayTaskA.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    long long k;
+    cout << "Entry num: \n";
+    cin >> k;
+
+    double commission = 25 + 0.01 * k; 
+    commission = max(100.0, min(commission, 2000.0)); 
+
+    cout << fixed << setprecision(2) << commission << endl; 
+
+    return 0;
+}
